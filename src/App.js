695176cc44acd5b1bb9f8215426919a68723ea9e
SaunakMalla7 <95901039+SaunakMalla7@users.noreply.github.com>
2022-12-19 17:29:57 +0545

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import { ToastContainer } from 'react-toastify';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer>
+        <h1>Hello</h1>
+      </ToastContainer>
     </div>
   );
-}
+};
 
 export default App;
